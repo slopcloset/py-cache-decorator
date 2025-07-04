ea639494b1e5a021fa346f8b5769f825fda68b86
slopcloset <51130108+slopcloset@users.noreply.github.com>
2025-07-04 12:23:01 -0400

    updated typing and added functools.wraps()

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    cache_dict = {}
-    
+    func_cache: Dict:[Tuple, Any] = {}
+
+    @wraps(func)
     def wrapper(*args, **kwargs):
         cache_key = (args, tuple(sorted(kwargs.items())))
-        if cache_key in cache_dict:
+        if cache_key in func_cache:
             print("Getting from cache")
-            return cache_dict[cache_key]
+            return func_cache[cache_key]
         else:
             print("Calculating new result")
-            cache_dict[cache_key] = func(*args, **kwargs)
-            return cache_dict[cache_key]
+            func_cache[cache_key] = func(*args, **kwargs)
+            return func_cache[cache_key]
     return wrapper
